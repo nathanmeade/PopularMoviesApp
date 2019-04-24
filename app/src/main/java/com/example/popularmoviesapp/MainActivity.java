d4f814c9418663bb0da5c293983b88f9fdb1f6f1
Nathan Meade <nathan.r.meade@gmail.com>
2019-04-23 21:04:24 -0400

    Revert "Second commit. Recommended changes have been made. Project has been adapted for Android Studio 3.4 as well."
    This reverts commit 6ef970365aaa8ca6830b970384567783b1d0c295.

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//Test comment for third commit. Using to differentiate commited files.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
