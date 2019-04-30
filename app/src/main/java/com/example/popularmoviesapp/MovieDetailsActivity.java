109aea5ea1921832be4028b2799c14430cbd89d4
Nathan Meade <nathan.r.meade@gmail.com>
2019-04-30 09:43:18 -0400

    Unique image successfully being displayed on the movie details screen.

--- app/src/main/java/com/example/popularmoviesapp/MovieDetailsActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MovieDetailsActivity.java
@@ -2,16 +2,21 @@ package com.example.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MovieDetailsActivity extends AppCompatActivity {
     private TextView textView;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
         textView = findViewById(R.id.text);
+        imageView = findViewById(R.id.image);
         String newString;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -24,5 +29,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
             newString= (String) savedInstanceState.getSerializable("string");
         }
         textView.setText(newString);
+        //imageView.setImageResource(R.mipmap.ic_launcher);
+        Picasso.get().load(newString).into(imageView);
     }
 }
