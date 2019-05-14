e44b7baca084652e921054ce330a301dee1a5722
Nathan Meade <nathan.r.meade@gmail.com>
2019-05-14 11:14:17 -0400

    Movie reviews successfully being retrieved and displayed on the movie details screen.

--- app/src/main/java/com/example/popularmoviesapp/MovieDetailsActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MovieDetailsActivity.java
@@ -4,13 +4,28 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MovieDetailsActivity extends AppCompatActivity {
     private TextView titleTextView;
@@ -19,6 +34,11 @@ public class MovieDetailsActivity extends AppCompatActivity {
     private TextView releaseDateTextView;
     private TextView movieIdTextView;
     private ImageView imageView;
+    private RecyclerView recyclerView;
+    private ReviewsRecyclerViewAdapter reviewsRecyclerViewAdapter;
+    private String jsonResponse;
+    private String url;
+    private String apiKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +49,10 @@ public class MovieDetailsActivity extends AppCompatActivity {
         releaseDateTextView = findViewById(R.id.release_date);
         imageView = findViewById(R.id.image);
         movieIdTextView = findViewById(R.id.movie_id);
+        recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
         String posterUrl;
         String title;
         String rating;
@@ -57,6 +81,10 @@ public class MovieDetailsActivity extends AppCompatActivity {
         releaseDateTextView.setText(releaseDate);
         movieIdTextView.setText(movieId);
         Picasso.get().load(posterUrl).into(imageView);
+        apiKey = BuildConfig.ApiKey;
+        url = "http://api.themoviedb.org/3/movie/" + movieId + "/reviews?api_key=" + apiKey;
+        new FetchReviewTask().execute(url);
+
     }
 
     public void onClickBtn(View v) {
@@ -71,6 +99,79 @@ public class MovieDetailsActivity extends AppCompatActivity {
             context.startActivity(appIntent);
         } catch (ActivityNotFoundException ex) {
             context.startActivity(webIntent);
+        }
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+
+    public class FetchReviewTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            try {
+                Uri uri = Uri.parse(strings[0]);
+                url = new URL(uri.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                jsonResponse = getResponseFromHttpUrl(url);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            JSONObject jsonObject;
+            JSONArray jsonArray;
+            JSONObject jsonObject2;
+            String reviewer;
+            ArrayList<String> reviewers = new ArrayList<String>();
+            String review;
+            ArrayList<String> reviews = new ArrayList<String>();
+            try {
+                jsonObject = new JSONObject(s);
+                jsonArray = jsonObject.getJSONArray(getString(R.string.results));
+                for (int i=0; i<jsonArray.length(); i++){
+                    jsonObject2 = jsonArray.getJSONObject(i);
+                    reviewer = jsonObject2.getString("author");
+                    review = jsonObject2.getString("content");
+                    reviewers.add(reviewer);
+                    reviews.add(review);
+                }
+                reviewsRecyclerViewAdapter = new ReviewsRecyclerViewAdapter(reviewers, reviews);
+                recyclerView.setAdapter(reviewsRecyclerViewAdapter);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
