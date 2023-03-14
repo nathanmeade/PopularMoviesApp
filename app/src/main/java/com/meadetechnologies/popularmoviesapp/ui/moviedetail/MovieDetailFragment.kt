69078456b925f6a628d6fdf75d700d89d85d2080
nathanmeade <nathan.r.meade@gmail.com>
2023-03-14 16:52:42 -0400

    Fragments now working with navigation components

--- app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/moviedetail/MovieDetailFragment.kt
@@ -4,20 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.meadetechnologies.popularmoviesapp.R
+import com.meadetechnologies.popularmoviesapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding : FragmentMovieDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_detail, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/movielist/MovieListFragment.kt
+++ app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/movielist/MovieListFragment.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.meadetechnologies.popularmoviesapp.R
+import com.meadetechnologies.popularmoviesapp.databinding.FragmentMovieListBinding
 
 class MovieListFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding : FragmentMovieListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_list, container, false)
+        binding.nextFragmentButton.setOnClickListener {
+            val action = MovieListFragmentDirections.actionMovieListFragmentToMovieDetailFragment()
+            findNavController().navigate(action)
+        }
+        return binding.root
     }
 
 }
