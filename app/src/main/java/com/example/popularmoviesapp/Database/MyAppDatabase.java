5da1e2bebfcdf81057a615d3ed1d323d3c8cda54
nathanmeade <nathan.r.meade@gmail.com>
2023-03-14 15:55:13 -0400

    Branch cleared for a new Android Studio project

--- app/src/main/java/com/example/popularmoviesapp/Database/MyAppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.popularmoviesapp.Database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Favorite.class},version = 1)
-public abstract class MyAppDatabase extends RoomDatabase {
-    public abstract MyDao myDao();
-}
