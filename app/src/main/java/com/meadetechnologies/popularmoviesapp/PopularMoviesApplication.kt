96d1e8d7d9835e2ed4a18b81d95366080e3391fe
nathanmeade <nathan.r.meade@gmail.com>
2023-03-15 11:27:59 -0400

    Custom application class implemented

---
+++ app/src/main/java/com/meadetechnologies/popularmoviesapp/PopularMoviesApplication.kt
@@ -0,0 +1,10 @@
+package com.meadetechnologies.popularmoviesapp
+
+import android.app.Application
+
+class PopularMoviesApplication : Application() {
+    override fun onCreate() {
+
+        super.onCreate()
+    }
+}
