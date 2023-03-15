eefc294f3e7939a4c6887eedab8bfa13501a9be4
nathanmeade <nathan.r.meade@gmail.com>
2023-03-15 12:09:45 -0400

    Movie id is now properly passed from MovieAdapter item to the MovieDetailFragment

--- app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/movielist/MovieAdapter.kt
+++ app/src/main/java/com/meadetechnologies/popularmoviesapp/ui/movielist/MovieAdapter.kt
@@ -1,10 +1,12 @@
 package com.meadetechnologies.popularmoviesapp.ui.movielist
 
+import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.Navigation
 import com.meadetechnologies.popularmoviesapp.R
 import com.meadetechnologies.popularmoviesapp.data.model.Movie
 
@@ -21,8 +23,14 @@ class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movie
             LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
         )
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.bind(movies[position])
+        val bundle = Bundle()
+        bundle.putInt("id", movies[position].id)
+        holder.itemView.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_movieListFragment_to_movieDetailFragment, bundle)
+        )
+    }
 
     override fun getItemCount() = movies.size
 
@@ -31,6 +39,9 @@ class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movie
             itemView.findViewById<TextView>(R.id.idTextView).text = item.id.toString()
 //            itemView.findViewById<TextView>(R.id.idTextView).setOnClickListener {  }
 //            itemView.findViewById<TextView>(R.id.idTextView).text = "test"
+            itemView.setOnClickListener {
+
+            }
         }
     }
 }
